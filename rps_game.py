1c0b7b5a7fde05f82b04494d0bf94c952103fb5c
Adam Fournace <fournacea@gmail.com>
2023-07-05 10:50:28 -0400

    ADD logic to determine win/lose

--- rps_game.py
+++ rps_game.py
@@ -1,3 +1,6 @@
+import random
+
+
 rock = '''
     _______
 ---'   ____)
@@ -25,6 +28,40 @@ scissors = '''
 ---.__(___)
 '''
 
+tie = "Tie!"
+win = "You win!"
+lose = "You lose!"
+
 #Write your code below this line 👇
 
+user_choice = input("Type 0 for Rock, 1 for Paper, or 2 for scissors")
 
+user_choice = int(user_choice)
+
+computer_choice = random.randint(0,2)
+
+print(user_choice, computer_choice)
+
+# Handle Ties
+if user_choice == 0 and computer_choice == 0:
+    print(tie)
+if user_choice == 1 and computer_choice == 1:
+    print(tie)
+if user_choice == 2 and computer_choice == 2:
+    print(tie)
+
+#Handle User Wins
+if user_choice == 0 and computer_choice == 2:
+    print(win)
+if user_choice == 1 and computer_choice == 0:
+    print(win)
+if user_choice == 2 and computer_choice == 1:
+    print(win)
+
+#Handle User Loses
+if user_choice == 0 and computer_choice == 1:
+    print(lose)
+if user_choice == 1 and computer_choice == 2:
+    print(lose)
+if user_choice == 2 and computer_choice == 0:
+    print(lose)
